53578b11ecec96fa3194a0b071be2153fcf39ec1
Nathan Jesudason <nathan.jesudason@gmail.com>
2018-09-19 09:32:51 -0700

    More Attempts at Class selection
    Avoiding using large Switchs.

--- ChromeExtension/popup.js
+++ ChromeExtension/popup.js
@@ -1,43 +1,43 @@
-chrome.browserAction.onClicked.addListener(function(tab) { Class = 1;
-chrome.storage.sync.get([Class], function(result) {
-document.getElementById('Students').value = result.key;
-names = result.key.split('\n');
-});
+chrome.browserAction.onClicked.addListener(function() { 
+	Class = 1;
+	chrome.storage.sync.get([Class], function(result) {
+		document.getElementById('Students').value = result.Class;
+		names = result.key.split('\n');
+		alert('successful load');
+	});
 });
 document.addEventListener('DOMContentLoaded', function() {
 
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
-    var text1 = document.getElementById('Students').value;
-    names = text1.split('\n');
-     chrome.storage.sync.set({Class: Number(document.getElementById("list").value)}, function() {
-          console.log('Value is set to ' + text1);
-        });
-        alert(text1);
-  }, false);
-  
-  var checkRanButton = document.getElementById('randomize');
-  checkRanButton.addEventListener('click', function() {
- 
-    
-var namesAsString = names.join(', ');
-document.getElementById("namelist").innerHTML = "Names: " + namesAsString;
-
-    var num = Math.floor(Math.random() * names.length);
-	document.getElementById("demo").innerHTML = names[num];
-  }, false);
-  
-  
-  
-  var SelectClassButton = document.getElementById('ClassSelect');
-  SelectClassButton.addEventListener('click', function() {
- Class = Number(document.getElementById("list").value);
- chrome.storage.sync.get([Class], function(result) {
-document.getElementById('Students').value = result.key;
-names = result.key.split('\n');
-});
-  }, false);
-  
+	var saveListButton = document.getElementById('saveList');
+	saveListButton.addEventListener('click', function() {
+		var text1 = document.getElementById('Students').value;
+		names = text1.split('\n');
+		Class = document.getElementById('list').value;
+		chrome.storage.sync.set({Class: text1}, function() {
+			alert('Value is set to ' + text1);
+		});
+		alert(text1);
+	}, false);
+	  
+	var checkRanButton = document.getElementById('randomize');
+	checkRanButton.addEventListener('click', function() {
+		var namesAsString = names.join(', ');
+		document.getElementById("namelist").innerHTML = "Names: " + namesAsString;
+		var num = Math.floor(Math.random() * names.length);
+		document.getElementById("demo").innerHTML = names[num];
+	}, false);
+	  
+	var SelectClassButton = document.getElementById('ClassSelect');
+	SelectClassButton.addEventListener('click', function() {
+		Class = document.getElementById('list').value;
+		alert(Class);
+		chrome.storage.sync.get([Class], function(result) {
+			document.getElementById('Students').value = result.Class;
+			names = result.key.split('\n');
+			alert('Class list has been changed.');
+		});
+		alert('An attempt was made to change class list'); 
+	}, false);
 }, false);
 
 
