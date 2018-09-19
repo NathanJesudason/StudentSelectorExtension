0386e4e979494bda76f1b1eb0e7397a88e1f14c2
NathanJesudason <nathan.jesudason@gmail.com>
2018-09-19 07:42:37 -0700

    Attempting to get different periods
    My attempt so far. Not quite working.

--- ChromeExtension/popup.js
+++ ChromeExtension/popup.js
@@ -1,13 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-chrome.storage.sync.get(['key'], function(result) {
+chrome.browserAction.onClicked.addListener(function(tab) { Class = 1;
+chrome.storage.sync.get([Class], function(result) {
 document.getElementById('Students').value = result.key;
 names = result.key.split('\n');
 });
+});
+document.addEventListener('DOMContentLoaded', function() {
+
   var checkPageButton = document.getElementById('checkPage');
   checkPageButton.addEventListener('click', function() {
     var text1 = document.getElementById('Students').value;
     names = text1.split('\n');
-     chrome.storage.sync.set({key: text1}, function() {
+     chrome.storage.sync.set({Class: Number(document.getElementById("list").value)}, function() {
           console.log('Value is set to ' + text1);
         });
         alert(text1);
@@ -23,4 +26,20 @@ document.getElementById("namelist").innerHTML = "Names: " + namesAsString;
     var num = Math.floor(Math.random() * names.length);
 	document.getElementById("demo").innerHTML = names[num];
   }, false);
+  
+  
+  
+  var SelectClassButton = document.getElementById('ClassSelect');
+  SelectClassButton.addEventListener('click', function() {
+ Class = Number(document.getElementById("list").value);
+ chrome.storage.sync.get([Class], function(result) {
+document.getElementById('Students').value = result.key;
+names = result.key.split('\n');
+});
+  }, false);
+  
 }, false);
+
+
+
+
