71d1051122aa581b3bc26aa659a76a61639e901f
NathanJesudason <nathan.jesudason@gmail.com>
2018-09-23 17:56:19 -0700

    Polish Extension
    Updating Index.html along with refining the UI.

--- ChromeExtension/popup.js
+++ ChromeExtension/popup.js
@@ -82,6 +82,7 @@ function selectClass(){
             chrome.storage.sync.get(['Period1'], function(result){
             console.log('successful load');
             document.getElementById('Students').value = result.Period1;
+            document.getElementById('displayClass').innerHTML = 'Students in Class 1';
             names = result.Period1.split('\n');
             });
 		break;
@@ -89,6 +90,7 @@ function selectClass(){
 		case 2:
 			chrome.storage.sync.get(['Period2'], function(result){
             document.getElementById('Students').value = result.Period2;
+            document.getElementById('displayClass').innerHTML = 'Currently Selected Class is Class 2';
             names = result.Period2.split('\n');
             console.log('successful load');
             });
@@ -96,6 +98,7 @@ function selectClass(){
 		case 3:
 			chrome.storage.sync.get(['Period3'], function(result){
             document.getElementById('Students').value = result.Period3;
+            document.getElementById('displayClass').innerHTML = 'Currently Selected Class is Class 3';
             names = result.Period3.split('\n');
             console.log('successful load');
             });
@@ -103,6 +106,7 @@ function selectClass(){
 		case 4:
 			chrome.storage.sync.get(['Period4'], function(result){
             document.getElementById('Students').value = result.Period4;
+            document.getElementById('displayClass').innerHTML = 'Currently Selected Class is Class 4';
             names = result.Period4.split('\n');
             console.log('successful load');
             });
@@ -110,6 +114,7 @@ function selectClass(){
 		case 5:
 			chrome.storage.sync.get(['Period5'], function(result){
             document.getElementById('Students').value = result.Period5;
+            document.getElementById('displayClass').innerHTML = 'Currently Selected Class is Class 5';
             names = result.Period5.split('\n');
             console.log('successful load');
             });
@@ -117,6 +122,7 @@ function selectClass(){
 		case 6:
 			chrome.storage.sync.get(['Period6'], function(result){
             document.getElementById('Students').value = result.Period6;
+            document.getElementById('displayClass').innerHTML = 'Currently Selected Class is Class 6';
             names = result.Period6.split('\n');
             console.log('successful load');
             });
@@ -124,6 +130,7 @@ function selectClass(){
 		case 7:
 			chrome.storage.sync.get(['Period7'], function(result){
             document.getElementById('Students').value = result.Period7;
+            document.getElementById('displayClass').innerHTML = 'Currently Selected Class is Class 7';
             names = result.Period7.split('\n');
             console.log('successful load');
             });
@@ -131,6 +138,7 @@ function selectClass(){
 		case 8:
 			chrome.storage.sync.get(['Period8'], function(result){
             document.getElementById('Students').value = result.Period8;
+            document.getElementById('displayClass').innerHTML = 'Currently Selected Class is Class 8';
             names = result.Period8.split('\n');
             console.log('successful load');
             });
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # StudentSelectorExtension
 This is a Google Chrome extension that selects a name from given lists at random
 # Created by team BNN News:
-![alt text](https://github.com/Benji-Wan-Kenobi/StudentSelectorExtension/blob/master/BNN%20News%20Logo.png)
+![alt text](https://github.com/Benji-Wan-Kenobi/StudentSelectorExtension/blob/master/BNN_News_Logo.png)
